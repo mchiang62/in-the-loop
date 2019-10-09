aeb2d5614cae009bc824ef8b38f9da721e5cb101
Ashley Clark <debashley06@gmail.com>
2019-10-09 16:17:00 -0400

    API call for weather, display table for weather

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,17 @@
-// Or with jQuery
-
-// Or with jQuery
- $(document).ready(function(){
+$(document).ready(function(){
     $('.carousel').carousel();
 
       
-  //initialization for collasping container
+//initialization for collasping container
+//collapse bar features
     $('.collapsible').collapsible();
     $(".results").hide();
-
     $("#search-button").on("click", function(){
       $(".results").show();
     });
-//need to create click event with images as well to show div
 
-
-  });
-$(document).ready(function () {
+//carousel features
   $('.carousel').carousel();
-
-
 
   $(".datepicker").datepicker({
     // add date range?
@@ -28,12 +20,37 @@ $(document).ready(function () {
 
   $("#search-button").on("click", function (event) {
     event.preventDefault();
-
      $("#search-event").val().trim();
      
   });
+//API call for weather 
+function weather(){
+  for (var i=0; i>5; i++){
+ // This is the API key for Open Weather
+ var APIKey = "b7b907c1b8d2d7c447d6c40de9d6cb86";
+ var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=atlanta,us&mode=json&units=imperial&APPID=" + APIKey
 
+ // AJAX call to the OpenWeatherMap API
+ $.ajax({
+   url: queryURL,
+   method: "GET"
+ })
+   .then(function(response) {
 
+// Log the resulting object
+  console.log(response);
 
+// Transfer content to HTML
+     $("#time").html("<p>" + response.list[i].dt);
+     $("#humidity").html("<p>" + "Humidity: " + response.list[i].main.humidity);
+     $("#temp").html("<p>" + response.list[i].main.temp + " degrees F");
+     $("#description").html("<p>" + response.list[i].weather[0].description);
+
+   
+
+});
+
+}}
+weather();
 
 });
