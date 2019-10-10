dfc6b50719319bcc3d8856e5ba644060083f4119
Melissa Chiang <mchiang62@gmail.com>
2019-10-10 17:34:26 -0400

    some links added

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,39 +1,67 @@
 // Or with jQuery
 
 // Or with jQuery
- $(document).ready(function(){
-    $('.carousel').carousel();
-
-      
-  //initialization for collasping container
-    $('.collapsible').collapsible();
-    $(".results").hide();
-
-    $("#search-button").on("click", function(){
-      $(".results").show();
-    });
-//need to create click event with images as well to show div
-
-
-  });
 $(document).ready(function () {
+  var firebaseConfig = {
+    apiKey: "AIzaSyCbdMUvQGzxT7u2VSA-tP09Jo6WgzqeNDA",
+    authDomain: "sunny-day-b106f.firebaseapp.com",
+    databaseURL: "https://sunny-day-b106f.firebaseio.com",
+    projectId: "sunny-day-b106f",
+    storageBucket: "",
+    messagingSenderId: "75744760770",
+    appId: "1:75744760770:web:4fe6c84e964a411276bc5b",
+    measurementId: "G-S16M0MNF0J"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+  var search;
+
+
   $('.carousel').carousel();
-
-
 
   $(".datepicker").datepicker({
     // add date range?
-
   });
 
-  $("#search-button").on("click", function (event) {
+
+  //initialization for collasping container
+  $('.collapsible').collapsible();
+  $(".results").hide();
+
+  $("#search-button").on("click", function () {
     event.preventDefault();
 
-     $("#search-event").val().trim();
-     
+    search = $("#search-event").val().trim();
+
+    database.ref().push ({
+
+      search: search, 
+
+
+    });
+
+
+
+
+    $(".results").show();
+  
+  
+  
+  
   });
+  //need to create click event with images as well to show div
+
 
 
 
 
 });
+
+    
+
+    
+
+ 
