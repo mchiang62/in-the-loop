7dfd15aa65bf248b871fa79b890d8c2c08d07b5e
Dan Gallagher <dagallagher221@gmail.com>
2019-10-10 20:19:53 -0400

    got the api to work and the date to work

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -41,8 +41,12 @@
 
     $(".results").show();
 
-
-    var queryURl = "https://api.stubhub.com/sellers/search/events/v3?q=" + search + "&city=Atlanta"
+    var currentDate = new Date();
+    var endDate = moment(currentDate).add(5, 'days').format("YYYY-MM-DD");
+    var startDate = moment(currentDate).format("YYYY-MM-DD");
+    console.log(startDate)
+    console.log(endDate)
+    var queryURl = "https://api.stubhub.com/sellers/search/events/v3?q=" + search + "&dateLocal=" + startDate +"TO" + endDate + "&city=Atlanta";
     $.ajax({
         method: "GET",
         url: queryURl,
@@ -51,9 +55,23 @@
         }
       })
       .then(function (response) {
-        var results = response.data;
-        console.log(results);
-        for (var i = 0; i < 5; i++) {}
+        var results = response;
+        console.log(results)
+        console.log(results.events.length)
+        for (var i = 0; i < results.events.length; i++) {
+          var eventName = results.events[i].name;
+          var eventVenue = results.events[i].venue.name;
+          var minTicketPrice = results.events[i].ticketInfo.minListPrice;
+          var maxTicketPrice = results.events[i].ticketInfo.maxListPrice;
+          var eventDate = results.events[i].eventDateLocal;
+            console.log(eventName)
+            console.log(eventVenue)
+            console.log(minTicketPrice)
+            console.log(maxTicketPrice)
+            console.log(eventDate)
+          
+          
+        }
       })
 
 
